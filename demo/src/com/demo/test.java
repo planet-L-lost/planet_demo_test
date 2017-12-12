0b7b3c676a7a92c81ed660193a3d5ae0430e02c9
planet-L-lost <34463929+planet-L-lost@users.noreply.github.com>
2017-12-12 11:10:53 +0800

    4

--- demo/src/com/demo/test.java
+++ demo/src/com/demo/test.java
@@ -3,6 +3,6 @@ package com.demo;
 public class test {
 	
 	public static void main(String[] args) {
-		System.out.println("23423432432423434");
+		System.out.println("oooooooooooooooooooooooo");
 	}
 }
